5f63c54019434e03340b0b1e5e440436d6984916
Mr_Lonely <94626014+Lonely371@users.noreply.github.com>
2023-10-22 20:39:02 +0800

    test button

--- vue/src/views/HomeView.vue
+++ vue/src/views/HomeView.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="home">
+    <el-button>This is a test button</el-button>
     <img alt="Vue logo" src="../assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
