b66d3028e62f0ff27ad10e7ff9dcba57d2abbbb3
Dragon4396 <113273923+Dragon4396@users.noreply.github.com>
2023-10-22 00:32:07 -0700

    Spring Boot framework setup

---
+++ src/test/java/com/example/springboot_administration_system/SpringbootAdministrationSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springboot_administration_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootAdministrationSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
