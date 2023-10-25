555bc27c8f204489a3ca523d2b1e222087712443
Dragon4396 <hekunlong2515@gmail.com>
2023-10-25 00:47:07 -0700

    Split Home

--- vue/src/views/Manage.vue
+++ vue/src/views/Manage.vue
@@ -2,51 +2,7 @@
   <el-container style="min-height: 100vh">
 
     <el-aside :width="sideWidth + 'px'" style="box-shadow: 2px 0 6px rgb(0 21 41 / 35%);">
-      <el-menu :default-openeds="['1', '3']" style="min-height: 100%; overflow-x: hidden"
-               background-color="rgb(48, 65, 86)"
-               text-color="#fff"
-               active-text-color="#ffd04b"
-               :collapse-transition="false"
-               :collapse="isCollapse"
-      >
-        <div style="height: 60px; line-height: 60px; text-align: center">
-          <img src="../assets/logo.png" alt="" style="width: 20px; position: relative; top: 5px; right: 5px">
-          <b style="color: white" v-show="logoTextShow">后台管理系统</b>
-        </div>
-        <el-submenu index="1">
-          <template slot="title">
-            <i class="el-icon-message"></i>
-            <span slot="title">导航一</span>
-          </template>
-          <el-menu-item-group title="分组2">
-            <el-menu-item index="1-3">选项3</el-menu-item>
-          </el-menu-item-group>
-          <el-submenu index="1-4">
-            <template slot="title">选项4</template>
-            <el-menu-item index="1-4-1">选项4-1</el-menu-item>
-          </el-submenu>
-        </el-submenu>
-        <el-submenu index="2">
-          <template slot="title">
-            <i class="el-icon-menu"></i>
-            <span slot="title">导航二</span>
-          </template>
-          <el-submenu index="2-4">
-            <template slot="title">选项4</template>
-            <el-menu-item index="2-4-1">选项4-1</el-menu-item>
-          </el-submenu>
-        </el-submenu>
-        <el-submenu index="3">
-          <template slot="title">
-            <i class="el-icon-setting"></i>
-            <span slot="title">导航三</span>
-          </template>
-          <el-submenu index="3-4">
-            <template slot="title">选项4</template>
-            <el-menu-item index="3-4-1">选项4-1</el-menu-item>
-          </el-submenu>
-        </el-submenu>
-      </el-menu>
+      <Aside :isCollapse="isCollapse" :logoTextShow="logoTextShow" />
     </el-aside>
 
     <el-container>
@@ -167,6 +123,8 @@
 
 <script>
 
+import Aside from "@/components/Aside";
+
 export default {
   name: 'HomeView',
   data() {
@@ -192,6 +150,9 @@ export default {
   created() {
     // 请求分页查询数据
     this.load()
+  },
+  components: {
+    Aside
   },
   methods: {
     collapse() {  // 点击收缩按钮触发
