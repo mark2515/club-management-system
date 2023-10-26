5caf03a08ba3fafee2b901716a71506e7f963414
Mr_Lonely <94626014+Lonely371@users.noreply.github.com>
2023-10-26 23:25:13 +0800

    export buttom

--- vue/src/views/User.vue
+++ vue/src/views/User.vue
@@ -22,7 +22,7 @@
         <el-button type="danger" slot="reference">批量删除 <i class="el-icon-remove-outline"></i></el-button>
       </el-popconfirm>
       <el-button type="primary" class="ml-5">导入 <i class="el-icon-bottom"></i></el-button>
-      <el-button type="primary">导出 <i class="el-icon-top"></i></el-button>
+      <el-button type="primary" @click="exp" class="ml-5">导出 <i class="el-icon-top"></i></el-button>
     </div>
 
     <el-table :data="tableData" border stripe :header-cell-class-name="'headerBg'"  @selection-change="handleSelectionChange">
@@ -177,6 +177,9 @@ export default {
       console.log(pageNum)
       this.pageNum = pageNum
       this.load()
+    },
+    exp() {
+	    window.open("http://localhost:9090/user/export")
     }
   }
 }
