93af2bf174c8d43b6b983bf1046494fce17c7a29
mark2515 <hekunlong2515@gmail.com>
2024-12-22 21:37:51 -0800

    edit index.html

--- vue/src/components/Aside.vue
+++ vue/src/components/Aside.vue
@@ -9,7 +9,7 @@
   >
     <div style="height: 60px; line-height: 60px; text-align: center">
       <img src="../assets/logo.png" alt="" style="width: 20px; position: relative; top: 5px; right: 5px">
-      <b style="color: white" v-show="logoTextShow">Administration</b>
+      <b style="color: white" v-show="logoTextShow">Club System</b>
     </div>
     <el-menu-item index="/home">
       <template slot="title">
