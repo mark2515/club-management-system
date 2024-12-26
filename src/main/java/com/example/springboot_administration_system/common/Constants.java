1c19b539f41262a3a0180e257aaff1e4c9829c93
Kelvin Li <94626014+Lonely371@users.noreply.github.com>
2024-12-25 21:01:27 -0800

    add code constants

---
+++ src/main/java/com/example/springboot_administration_system/common/Constants.java
@@ -0,0 +1,7 @@
+package com.example.springboot_administration_system.common;
+
+public interface Constants {
+    String CODE_200 = "200";
+    String CODE_401 = "401";
+    String CODE_500 = "500";
+}
